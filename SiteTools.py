331038cdb82d28f5dda3ef1a68a716f60813abec
Clay Curry <claycurry34@ou.edu>
2021-02-23 10:08:49 -0600

    generating a list of status codes

--- SiteTools.py
+++ SiteTools.py
@@ -1,23 +1,23 @@
 import os
 import re
+import requests
 from typing import List
+from html.parser import HTMLParser
+
+#import webbrowser webbrowser.open(link)
 
 
-#for item in items:
-#   isDir = "directory" if os.path.isdir(item) else "file"
-#    print("" + item + " is a " + isDir)
 
 #######################################################################
-#  Make a list broken links, together with their addresses
+#  Want to generate a list broken links on the entire site, together with their addresses
 #######################################################################
 # 1. Given a file system, want to expand directories recursively, returning a list of addresses
 #   to each file
-def expandDir(dir : List[str]) -> List[str]:
+def expandDir(dir : List[str]) -> List[str]: # Wrapper function
     return sorted(__expandDir(dir), key=str.casefold)
-
 def __expandDir(dir : List[str]) -> List[str]:
     for item in dir:            # checking each item in directory
-        if os.path.isdir(item):     # if some item is a directory,
+        if os.path.isdir(item):     # if item is a directory, expand directory recursively
             os.chdir(item)
             subdir = os.listdir()
             files = [item + "/" + s for s in expandDir(subdir)]
@@ -49,20 +49,61 @@ def filterHTML(files, exclude = ".*?\\.(js|jpg|pdf)"): #-> List[bool]:
             pass
         if(response == None):
             del files[i]
-
-
     return files
 
 
-# 3. Adds HTML files to a dict mapping to list of <href> attributes { File: List of hrefs }
+
+# 3. Create a mapping from <href> attributes to the files contining it { href : List of files containing href }
 #   Adds <hrefs> attributes to a dict { href: isWorking } initialized to FALSE,
 
-def generateDicts():
+href_to_files_map = {}
+href_IsWorking_map = {}
+class HTML_Parser(HTMLParser):
+    hrefs = set()
+    def __init__(self, file : str, include = None):
+        super().__init__()
+        self.domain = re.compile("https?://(www\\.)?((?!nhn\\.ou.edu).)*$", re.IGNORECASE)
+        self.file = file
+        text = open(file, "r")
+        self.feed(text.read()) # calls parse method on HTML file
+        text.close()
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":                                                       # filter <a > tags
+            for attr in attrs:                                               # checking for <href> tag attributes
+                if attr[0] == "href" and self.domain.match(attr[1]) != None: # excludes www.nhn.ou.edu/ <href> tags
+                    if attr[1] in href_to_files_map.keys():
+                        currentMapping = href_to_files_map[attr[1]]
+                        currentMapping.append(self.file)
+                        href_to_files_map[attr[1]] = sorted(currentMapping, key=str.casefold)  # adds to existing maps
 
-    file_Hrefs_map = {}
-    href_isWorking_map = {}
+                    else:
+                        href_IsWorking_map[attr[1]] = None                  # creates new <href> -> boolean map
+                        href_to_files_map[attr[1]] = [self.file]             # creates new <href> -> list of files containing <href> map
+def scanHrefs(files : List[str], ignore = None):
+    for file in files:
+        HTML_Parser(file)
 
+def get_href_IsWorking_map():
+    return href_IsWorking_map
+def get_href_to_files_map():
+    return href_to_files_map
 # 4. Confirm whether the hrefs are working
+
+def confirmWorking():
+    ct = 0
+    for key in href_IsWorking_map.keys():
+        if (ct % 20 == 0):
+            print("Checked status of " + str(ct) + " unique external links")
+            print("Progress : " + str(100*round(ct/len(href_IsWorking_map),2)) + "%")
+        ct+=1
+        try:
+            r = requests.get(key, headers={
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"})
+            href_IsWorking_map[key] = str(r.history)+str(r.status_code)
+        except:
+            href_IsWorking_map[key] = "ERR"
+
 
 # 5. Iterates through file_Hrefs_map, printing each file together with the broken resource
 def printBrokenResources():
--- __main__.py
+++ __main__.py
@@ -1,12 +1,21 @@
 import os
 from Physics.SiteTools import *
-
+#from Physics.SiteTools import expandDir, filterHTML
 
 os.chdir("Physics/www.nhn.ou.edu")
+items = expandDir(os.listdir())    # 1. gets path of all files
+items = filterHTML(items)          # 2. reads each file to determine HTML
+scanHrefs(items, ignore="(@|/|https://www.nhn.ou.edu/|http://www.nhn.ou.edu/|\\.jpg\\b|\\.pdf\\b)")
 
-items = os.listdir()
-items = expandDir(items)    # 1. gets path of all files
+hrefs_isWorking_map = get_href_IsWorking_map()
+href_to_files_map = get_href_to_files_map()
+print("Discovered " +
+      str(len(items)) +
+      " unique HTML files with domain www.nhn.ou.edu" +
+      " and " +
+      str(len(href_IsWorking_map.keys())) +
+      " unique external links")
 
-#items = filterHTML(["assets/alum-photos/friedman-bike.jpg"])
-items = filterHTML(items)   # 2. reads each of 1. and removes nonhtml
-print(items)
+confirmWorking()
+os.chdir("..")
+print(href_IsWorking_map, file=open("hrefs.txt", "a"))
